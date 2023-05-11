e069c4f005a04616d3957d9cd5db94a14cf1000b
kudai68 <jamiylnkunda@gmail.com>
2023-05-11 19:05:27 +0200

    Création d'une liste de 10 éléments

---
+++ main.py
@@ -0,0 +1,2 @@
+# Création d'une liste de 10 éléments de type chaîne de caractères
+liste = ["Football", "Basketball", "Handball", "Volleyball", "Baseball", "Hockey", "Golf", "Cycling", "Cricket", "F1"]
