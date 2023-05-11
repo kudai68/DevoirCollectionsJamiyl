c7f9dbcbf7e101b8c9036435eebebfd19f509dd2
kudai68 <jamiylnkunda@gmail.com>
2023-05-11 19:07:19 +0200

    Affichage des éléments de la liste

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 # Création d'une liste de 10 éléments de type chaîne de caractères
 liste = ["Football", "Basketball", "Handball", "Volleyball", "Baseball", "Hockey", "Golf", "Cycling", "Cricket", "F1"]
+
+# Affichage des éléments de la liste
+print("----------------------------------Affichage des éléments de la liste----------------------------------------")
+print(liste)
